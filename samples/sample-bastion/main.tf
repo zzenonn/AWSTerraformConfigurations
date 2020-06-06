2c1e102b05da2f3e1c1fd25b162c67d65f76e57f
zzenonn <ubuntu@ip-172-31-23-124.ap-southeast-1.compute.internal>
2020-06-06 09:56:10 +0000

    Fix path

--- samples/sample-bastion/main.tf
+++ samples/sample-bastion/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "network" {
-    source          = "../../modules/network"
+    source          = "../../modules/infrastructure/network"
     project_name    = var.project_name
     environment     = var.environment
     db_port         = var.db_port
@@ -12,7 +12,7 @@ module "network" {
 }
 
 module "instances" {
-    source  = "../../modules/instances"
+    source  = "../../modules/infrastructure/instances"
     project_name    = module.network.project_name
     environment     = module.network.environment
     db_port         = module.network.db_port
