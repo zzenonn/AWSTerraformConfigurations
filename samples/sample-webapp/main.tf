ea683846ff119e16c89a005901dfce7a012f7a8f
zzenonn <miguel.saavedra@obf.ateneo.edu>
2022-09-05 10:49:30 +0800

    Update userdata

--- samples/sample-webapp/main.tf
+++ samples/sample-webapp/main.tf
@@ -33,29 +33,16 @@ module "webapp" {
       #Install nodejs
       yum -y install nodejs
 
-      #Install Amazon Linux extras
-      amazon-linux-extras install epel
-
-      #Install stress tool (for load balancing testing)
-      yum -y install stress
-
       # Create a dedicated directory for the application
       mkdir -p /var/app
 
-      # Get the app from Amazon S3
+      # Get the app from S3
       wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/ILT-TF-100-TECESS-5/app/app.zip
 
-      # Extract it into a desired folder
+      # Unzip it into a specific folder
       unzip app.zip -d /var/app/
       cd /var/app/
 
-      # Configure S3 bucket details
-      export PHOTOS_BUCKET=YOUR-S3-BUCKET-NAME
-
-      # Configure default AWS Region
-      export DEFAULT_AWS_REGION=YOUR-DEFAULT-AWS-REGION
-
-      # Enable admin tools for stress testing
       export SHOW_ADMIN_TOOLS=1
 
       # Install dependencies
@@ -63,6 +50,7 @@ module "webapp" {
 
       # Start your app
       npm start
+
 
     EOF
 }
