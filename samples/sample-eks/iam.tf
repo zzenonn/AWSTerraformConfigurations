b98c76fa99e25954fe4315e7e7449ef1d443a698
zzenonn <miguel.saavedra@obf.ateneo.edu>
2024-07-06 17:21:02 +0800

    Add APIGW controller IAM

--- samples/sample-eks/iam.tf
+++ samples/sample-eks/iam.tf
@@ -1,5 +1,6 @@
-# IAM For Cluster
+# IAM Roles and Policies for EKS Cluster and Node Group
 
+# IAM Role for EKS Cluster
 resource "aws_iam_role" "cluster_role" {
   name = "${local.name_tag_prefix}-EKS-Cluster-Role"
 
@@ -19,6 +20,7 @@ resource "aws_iam_role" "cluster_role" {
 POLICY
 }
 
+# Attach Policies to EKS Cluster Role
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster_role.name
@@ -29,16 +31,12 @@ resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicyCluster" {
   role       = aws_iam_role.cluster_role.name
 }
 
-
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.cluster_role.name
 }
 
-# IAM For Node Group
-
+# IAM Role for EKS Node Group
 resource "aws_iam_role" "node_role" {
   name = "${local.name_tag_prefix}-EKS-Node-Role"
 
@@ -54,42 +52,44 @@ resource "aws_iam_role" "node_role" {
   })
 }
 
+# Node Group SSM Policy
 resource "aws_iam_role_policy" "node_ssm" {
   role   = aws_iam_role.node_role.id
-  policy = <<-EOF
+  policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssm:UpdateInstanceInformation",
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel",
-                "autoscaling:DescribeAutoScalingGroups",
-                "autoscaling:DescribeAutoScalingInstances",
-                "autoscaling:DescribeLaunchConfigurations",
-                "autoscaling:DescribeTags",
-                "autoscaling:SetDesiredCapacity",
-                "autoscaling:TerminateInstanceInAutoScalingGroup",
-                "ec2:DescribeLaunchTemplateVersions"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetEncryptionConfiguration"
-            ],
-            "Resource": "*"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:UpdateInstanceInformation",
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel",
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "autoscaling:SetDesiredCapacity",
+        "autoscaling:TerminateInstanceInAutoScalingGroup",
+        "ec2:DescribeLaunchTemplateVersions"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetEncryptionConfiguration"
+      ],
+      "Resource": "*"
+    }
+  ]
 }
-  EOF
+EOF
 }
 
+# Attach Policies to EKS Node Role
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.node_role.name
@@ -110,7 +110,7 @@ resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicyWorker" {
   role       = aws_iam_role.node_role.name
 }
 
-# OIDC allowing K8s pods to assume IAM roles
+# OIDC Provider for EKS Cluster
 data "tls_certificate" "eks_cluster" {
   url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
@@ -121,7 +121,7 @@ resource "aws_iam_openid_connect_provider" "oidc" {
   url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
 
-# Assume role policy for the ALB controller service account
+# ALB Controller IAM Role
 data "aws_iam_policy_document" "alb_controller_sa_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -146,7 +146,21 @@ data "aws_iam_policy_document" "alb_controller_sa_assume_role_policy" {
   }
 }
 
-# Assume role policy for the gateway controller service account
+resource "aws_iam_role" "kube_alb_controller" {
+  assume_role_policy = data.aws_iam_policy_document.alb_controller_sa_assume_role_policy.json
+  name               = "${local.name_tag_prefix}-Kube-Alb-Controller-Role"
+  tags = {
+    Env     = var.environment
+    Project = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy" "kube_alb_controller" {
+  role   = aws_iam_role.kube_alb_controller.id
+  policy = file("${path.module}/elb-controller-policy.json")
+}
+
+# Gateway Controller IAM Role
 data "aws_iam_policy_document" "gateway_controller_sa_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -171,7 +185,21 @@ data "aws_iam_policy_document" "gateway_controller_sa_assume_role_policy" {
   }
 }
 
-# IAM Policy for the EBS CSI controller service account
+resource "aws_iam_role" "kube_gateway_controller" {
+  assume_role_policy = data.aws_iam_policy_document.gateway_controller_sa_assume_role_policy.json
+  name               = "${local.name_tag_prefix}-Kube-Gateway-Controller-Role"
+  tags = {
+    Env     = var.environment
+    Project = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy" "kube_gateway_controller" {
+  role   = aws_iam_role.kube_gateway_controller.id
+  policy = file("${path.module}/gateway-controller.json")
+}
+
+# EBS CSI Controller IAM Role
 data "aws_iam_policy_document" "ebs_csi_controller_sa_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -196,38 +224,6 @@ data "aws_iam_policy_document" "ebs_csi_controller_sa_assume_role_policy" {
   }
 }
 
-
-# IAM Policy for the ALB controller service account
-resource "aws_iam_role_policy" "kube_alb_controller" {
-  role   = aws_iam_role.kube_alb_controller.id
-  policy = file("${path.module}/elb-controller-policy.json")
-}
-
-resource "aws_iam_role" "kube_alb_controller" {
-  assume_role_policy = data.aws_iam_policy_document.alb_controller_sa_assume_role_policy.json
-  name               = "${local.name_tag_prefix}-Kube-Alb-Controller-Role"
-  tags = {
-    Env     = var.environment
-    Project = var.project_name
-  }
-}
-
-# IAM Policy for the gateway controller service account
-
-resource "aws_iam_role_policy" "kube_gateway_controller" {
-  role   = aws_iam_role.kube_gateway_controller.id
-  policy = file("${path.module}/gateway-controller.json")
-}
-
-resource "aws_iam_role" "kube_gateway_controller" {
-  assume_role_policy = data.aws_iam_policy_document.gateway_controller_sa_assume_role_policy.json
-  name               = "${local.name_tag_prefix}-Kube-Gateway-Controller-Role"
-  tags = {
-    Env     = var.environment
-    Project = var.project_name
-  }
-}
-
 resource "aws_iam_role" "kube_ebs_csi_controller" {
   assume_role_policy = data.aws_iam_policy_document.ebs_csi_controller_sa_assume_role_policy.json
   name               = "${local.name_tag_prefix}-Kube-EBS-CSI-Controller-Role"
@@ -240,4 +236,43 @@ resource "aws_iam_role" "kube_ebs_csi_controller" {
 resource "aws_iam_role_policy_attachment" "ebs_csi_policy_attachment" {
   role       = aws_iam_role.kube_ebs_csi_controller.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# APIGW Controller IAM Role
+data "aws_iam_policy_document" "apigw_controller_sa_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ack-apigatewayv2-controller"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.oidc.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "kube_apigw_controller" {
+  assume_role_policy = data.aws_iam_policy_document.apigw_controller_sa_assume_role_policy.json
+  name               = "${local.name_tag_prefix}-Kube-ACKIAM-Role"
+  tags = {
+    Env     = var.environment
+    Project = var.project_name
+  }
+}
+
+resource "aws_iam_role_policy" "kube_apigw_controller" {
+  role   = aws_iam_role.kube_apigw_controller.id
+  policy = file("${path.module}/ack-iam-policy.json")
 }
