431db53cb5990084d89794879116edf70ab2f97f
zzenonn <zenon@zaavedra.xyz>
2024-11-20 23:48:59 +0800

    Remove DB endpoint from output

--- modules/infrastructure/instances/outputs.tf
+++ modules/infrastructure/instances/outputs.tf
@@ -2,9 +2,9 @@ output "bastion_id" {
   value = aws_instance.bastion.id
 }
 
-output "db_endpoint" {
-  value = aws_db_instance.db.endpoint
-}
+# output "db_endpoint" {
+#   value = aws_db_instance.db.endpoint
+# }
 
 output "ssm_role" {
   value = aws_iam_role.ssm_role.arn
