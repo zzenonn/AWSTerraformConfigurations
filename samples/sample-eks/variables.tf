1971ae4479c43158fd8577ab6f449f8066ef857e
zzenonn <zzenonn>
2026-03-19 04:18:32 +0000

    Fix EKS sample deprecations and update 2048 game image
    - Replace alexwhen/docker-2048 with sevenajay/2048 for containerd compatibility
    - Add elasticloadbalancing:DescribeListenerAttributes to ALB controller policies
    - Replace deprecated aws_region name attribute with region
    - Replace deprecated filter block in aws_ec2_managed_prefix_list with name argument

--- samples/sample-eks/variables.tf
+++ samples/sample-eks/variables.tf
@@ -218,8 +218,5 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_ec2_managed_prefix_list" "vpc_lattice" {
-  filter {
-    name   = "prefix-list-name"
-    values = ["com.amazonaws.${data.aws_region.current.name}.vpc-lattice"]
-  }
+  name = "com.amazonaws.${data.aws_region.current.region}.vpc-lattice"
 }
